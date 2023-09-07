83c207f4adb7b8b42c234ac9b8d825bb53520666
LookeiCode <137450026+LookeiCode@users.noreply.github.com>
2023-09-06 21:21:23 -0400

    Added functionality - clicking a puppy name displays info about the puppy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
-import {puppyList} from './data.js'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
 
-  console.log("Puppy List: ", puppyList)
+  // console.log("Featured Pup: ", featuredPup)
+  // console.log("Puppy List: ", puppyList)
 
   return (
     <div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       {
-         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+        puppies.map((puppy) => {
+          return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
         })
       }
     </div>
